7fe62416d1b322fd519c93b15b2eeb38b2542f54
Ankit Kumar Pandit <panditankit1995@gmail.com>
2024-04-10 20:19:29 +0530

    date-time - diffInDays added

--- packages/date-time/src/index.ts
+++ packages/date-time/src/index.ts
@@ -1,4 +1,6 @@
+export * from "./date-before-days";
 export * from "./date";
+export * from "./diff-in-days";
 export * from "./seconds-from-previous";
 export * from "./seconds-to-expiry";
 export * from "./time-in-seconds";
